5c7d47fffd382e8f2a937bf71971a5a58a44fd84
EmilianoHFlores <A00833160@tec.mx>
2023-02-28 14:56:56 -0600

    Added find_w_face function
    Reduces extraction_face repetitions.

--- deepface/DeepFace.py
+++ deepface/DeepFace.py
@@ -12,7 +12,6 @@ import pandas as pd
 from tqdm import tqdm
 import cv2
 import tensorflow as tf
-from deprecated import deprecated
 
 # package dependencies
 from deepface.basemodels import (
@@ -28,7 +27,6 @@ from deepface.basemodels import (
 )
 from deepface.extendedmodels import Age, Gender, Race, Emotion
 from deepface.commons import functions, realtime, distance as dst
-
 
 # -----------------------------------
 # configurations for dependencies
@@ -486,7 +484,7 @@ def find(
         )
         for index in pbar:
             employee = employees[index]
-
+            print(f"Finding representations for {employee}")
             img_objs = functions.extract_faces(
                 img=employee,
                 target_size=target_size,
@@ -598,6 +596,219 @@ def find(
 
     return resp_obj
 
+# Optimized to use face object fromm extract_face
+def find_w_face(
+    face,
+    db_path,
+    model_name="VGG-Face",
+    distance_metric="cosine",
+    enforce_detection=True,
+    detector_backend="opencv",
+    align=True,
+    normalization="base",
+    silent=False,
+):
+
+    """
+    This function applies verification several times and find the identities in a database
+
+    Parameters:
+            img_path: exact image path, numpy array (BGR) or based64 encoded image.
+            Source image can have many faces. Then, result will be the size of number of
+            faces in the source image.
+
+            db_path (string): You should store some image files in a folder and pass the
+            exact folder path to this. A database image can also have many faces.
+            Then, all detected faces in db side will be considered in the decision.
+
+            model_name (string): VGG-Face, Facenet, Facenet512, OpenFace, DeepFace, DeepID,
+            Dlib, ArcFace, SFace or Ensemble
+
+            distance_metric (string): cosine, euclidean, euclidean_l2
+
+            enforce_detection (bool): The function throws exception if a face could not be detected.
+            Set this to True if you don't want to get exception. This might be convenient for low
+            resolution images.
+
+            detector_backend (string): set face detector backend to opencv, retinaface, mtcnn, ssd,
+            dlib or mediapipe
+
+            silent (boolean): disable some logging and progress bars
+
+    Returns:
+            This function returns list of pandas data frame. Each item of the list corresponding to
+            an identity in the img_path.
+    """
+
+    tic = time.time()
+
+    # -------------------------------
+    if os.path.isdir(db_path) is not True:
+        raise ValueError("Passed db_path does not exist!")
+
+    target_size = functions.find_target_size(model_name=model_name)
+
+    # ---------------------------------------
+
+    file_name = f"representations_{model_name}.pkl"
+    file_name = file_name.replace("-", "_").lower()
+
+    if path.exists(db_path + "/" + file_name):
+
+        if not silent:
+            print(
+                f"WARNING: Representations for images in {db_path} folder were previously stored"
+                + f" in {file_name}. If you added new instances after the creation, then please "
+                + "delete this file and call find function again. It will create it again."
+            )
+
+        with open(f"{db_path}/{file_name}", "rb") as f:
+            representations = pickle.load(f)
+
+        if not silent:
+            print("There are ", len(representations), " representations found in ", file_name)
+
+    else:  # create representation.pkl from scratch
+        employees = []
+
+        for r, _, f in os.walk(db_path):
+            for file in f:
+                if (
+                    (".jpg" in file.lower())
+                    or (".jpeg" in file.lower())
+                    or (".png" in file.lower())
+                ):
+                    exact_path = r + "/" + file
+                    employees.append(exact_path)
+
+        if len(employees) == 0:
+            raise ValueError(
+                "There is no image in ",
+                db_path,
+                " folder! Validate .jpg or .png files exist in this path.",
+            )
+
+        # ------------------------
+        # find representations for db images
+
+        representations = []
+
+        # for employee in employees:
+        pbar = tqdm(
+            range(0, len(employees)),
+            desc="Finding representations",
+            disable=silent,
+        )
+        for index in pbar:
+            employee = employees[index]
+            print(f"Finding representations for {employee}")
+            img_objs = functions.extract_faces(
+                img=employee,
+                target_size=target_size,
+                detector_backend=detector_backend,
+                grayscale=False,
+                enforce_detection=enforce_detection,
+                align=align,
+            )
+
+            for img_content, _, _ in img_objs:
+                embedding_obj = represent(
+                    img_path=img_content,
+                    model_name=model_name,
+                    enforce_detection=enforce_detection,
+                    detector_backend="skip",
+                    align=align,
+                    normalization=normalization,
+                )
+
+                img_representation = embedding_obj[0]["embedding"]
+
+                instance = []
+                instance.append(employee)
+                instance.append(img_representation)
+                representations.append(instance)
+
+        # -------------------------------
+
+        with open(f"{db_path}/{file_name}", "wb") as f:
+            pickle.dump(representations, f)
+
+        if not silent:
+            print(
+                f"Representations stored in {db_path}/{file_name} file."
+                + "Please delete this file when you add new identities in your database."
+            )
+
+    # ----------------------------
+    # now, we got representations for facial database
+    print("got representations")
+    df = pd.DataFrame(representations, columns=["identity", f"{model_name}_representation"])
+
+    resp_obj = []
+
+    print ("entering faces loop")
+
+    target_img = face["face"]
+    target_region = face["facial_area"]
+
+    print("representing")
+    target_embedding_obj = represent(
+        img_path=target_img,
+        model_name=model_name,
+        enforce_detection=enforce_detection,
+        detector_backend="skip",
+        align=align,
+        normalization=normalization,
+    )
+    print("represented")
+
+    target_representation = target_embedding_obj[0]["embedding"]
+
+    result_df = df.copy()  # df will be filtered in each img
+    result_df["source_x"] = target_region["x"]
+    result_df["source_y"] = target_region["y"]
+    result_df["source_w"] = target_region["w"]
+    result_df["source_h"] = target_region["h"]
+
+    distances = []
+    for index, instance in df.iterrows():
+        source_representation = instance[f"{model_name}_representation"]
+
+        if distance_metric == "cosine":
+            distance = dst.findCosineDistance(source_representation, target_representation)
+        elif distance_metric == "euclidean":
+            distance = dst.findEuclideanDistance(source_representation, target_representation)
+        elif distance_metric == "euclidean_l2":
+            distance = dst.findEuclideanDistance(
+                dst.l2_normalize(source_representation),
+                dst.l2_normalize(target_representation),
+            )
+        else:
+            raise ValueError(f"invalid distance metric passes - {distance_metric}")
+
+        distances.append(distance)
+
+        # ---------------------------
+
+    result_df[f"{model_name}_{distance_metric}"] = distances
+
+    threshold = dst.findThreshold(model_name, distance_metric)
+    result_df = result_df.drop(columns=[f"{model_name}_representation"])
+    result_df = result_df[result_df[f"{model_name}_{distance_metric}"] <= threshold]
+    result_df = result_df.sort_values(
+        by=[f"{model_name}_{distance_metric}"], ascending=True
+    ).reset_index(drop=True)
+
+    resp_obj.append(result_df)
+
+    # -----------------------------------
+
+    toc = time.time()
+
+    if not silent:
+        print("find function lasts ", toc - tic, " seconds")
+
+    return resp_obj
 
 def represent(
     img_path,
@@ -808,59 +1019,6 @@ def extract_faces(
         resp_objs.append(resp_obj)
 
     return resp_objs
-
-
-# ---------------------------
-# deprecated functions
-
-
-@deprecated(version="0.0.78", reason="Use DeepFace.extract_faces instead of DeepFace.detectFace")
-def detectFace(
-    img_path, target_size=(224, 224), detector_backend="opencv", enforce_detection=True, align=True
-):
-    """
-    Deprecated function. Use extract_faces for same functionality.
-    
-    This function applies pre-processing stages of a face recognition pipeline
-    including detection and alignment
-
-    Parameters:
-            img_path: exact image path, numpy array (BGR) or base64 encoded image.
-            Source image can have many face. Then, result will be the size of number
-            of faces appearing in that source image.
-
-            target_size (tuple): final shape of facial image. black pixels will be
-            added to resize the image.
-
-            detector_backend (string): face detection backends are retinaface, mtcnn,
-            opencv, ssd or dlib
-
-            enforce_detection (boolean): function throws exception if face cannot be
-            detected in the fed image. Set this to False if you do not want to get
-            an exception and run the function anyway.
-
-            align (boolean): alignment according to the eye positions.
-
-            grayscale (boolean): extracting faces in rgb or gray scale
-
-    Returns:
-            detected and aligned face as numpy array
-            
-    """
-    print("⚠️ Function detectFace is deprecated. Use extract_faces instead.")
-    face_objs = extract_faces(
-        img_path=img_path,
-        target_size=target_size,
-        detector_backend=detector_backend,
-        enforce_detection=enforce_detection,
-        align=align,
-        grayscale=False,
-    )
-
-    extracted_face = None
-    if len(face_objs) > 0:
-        extracted_face = face_objs[0]["face"]
-    return extracted_face
 
 
 # ---------------------------
